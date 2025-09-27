bd9972e07c7079d963cb1a485d7da83d8ea5888f
TaichaMu <mu.taicha@gmail.com>
2025-09-27 21:21:51 +0900

    reset

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ export function startMain(container) {
 
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(container.clientWidth, container.clientHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
     container.appendChild(renderer.domElement);
 
     // 光源
