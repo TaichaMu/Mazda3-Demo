ceeb3f8b4bf1f7b538bad252cc9a38339d304f9c
TaichaMu <mu.taicha@gmail.com>
2025-09-27 21:09:32 +0900

    fix: style 3

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ export function startMain(container) {
     );
 
     const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(container.clientWidth, container.clientHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
     container.appendChild(renderer.domElement);
 
